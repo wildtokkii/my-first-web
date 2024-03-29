382716ac756eac7654e67ca749110afcc43da76c
wildtokkii <wildtokkii@gmail.com>
2024-03-29 21:40:15 +0900

    생활코딩 Javascript 복습

---
+++ 06_javascript_remind/colors.js
@@ -0,0 +1,38 @@
+var Links = {
+    setColor:function(color){
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i < alist.length){
+            alist[i].style.color = color ;
+            i = i + 1;
+        }  
+    }
+
+}
+var Body = {
+    setColor:function(color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function(color){
+        document.querySelector('body').style.backgroundColor= color;
+    }
+
+}
+
+function nightDayHandler(self){
+    var target =  document.querySelector('body');
+    if (self.value === '다크 모드'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value  = '라이트 모드';
+
+        Links.setColor('white');
+    }else{
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value  = '다크 모드';
+
+
+        Links.setColor('black');
+    } 
+ }
